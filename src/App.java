dbbc7dfa032e0d4c9afba218e3b16837a6076605
xp4w <benzilla09.xbl@gmail.com>
2023-09-14 20:33:03 -0500

    i forgor to commit a while ago

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int windowWidth = 600;
+        int windowHeight = 600;
+
+        JFrame window = new JFrame("Snake");
+        window.setVisible(true);
+        window.setSize(windowWidth, windowHeight);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/Snake.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class Snake extends JPanel{
+    int windowWidth;
+    int windowHeight;
+
+    Snake(int windowWidth, int windowHeight) {
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
+    }
+}
