61bcbe2fed749ebee8afb25747bd6c318fbd1edd
xp4w <benzilla09.xbl@gmail.com>
2023-09-14 20:47:55 -0500

    made snake head (so it can give head)

--- src/Snake.java
+++ src/Snake.java
@@ -5,13 +5,37 @@ import java.util.Random;
 import javax.swing.*;
 
 public class Snake extends JPanel{
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     int windowWidth;
     int windowHeight;
+    int tileSize = 25;
+
+    Tile snakeHead;
 
     Snake(int windowWidth, int windowHeight) {
         this.windowWidth = windowWidth;
         this.windowHeight = windowHeight;
         setPreferredSize(new Dimension(this.windowWidth, this.windowHeight));
         setBackground(Color.black);
+
+        snakeHead = new Tile(5, 5);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x, snakeHead.y, tileSize, tileSize);
     }
 }
